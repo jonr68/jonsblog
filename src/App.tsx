b2b5f8c8a263b1a66272995dce5d74657ae0532c
jonr68 <jonathanerhine@gmail.com>
2024-12-25 13:41:26 -0600

    Built out navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,47 @@ import TestBlogs from "./Data/TestBloggs";
 function App() {
   return (
     <>
-      <div>
-        {TestBlogs.map((blog, index) => (
-          <div key={index}>
-            <div>
-              <div> {blog.title} </div>
-              <div> {blog.body}</div>
-              <div> {blog.publishDate}</div>
-              <div> {blog.author}</div>
+      <body>
+        <header>
+          <nav
+            className={"navBar"}
+            role="navigation"
+            aria-label="main navigation"
+          >
+            <div className="navbarContainer">
+              <ul className="navbarMenu">
+                <li className="navbarItem">
+                  <a className="navbarLink" href="/">
+                    Home
+                  </a>
+                </li>
+                <li className="navbarItem">
+                  <a className="navbarLink" href="/blogs">
+                    BLOGS
+                  </a>
+                </li>
+                <li className="navbarItem">
+                  <a className="navbarLink" href="/newblog">
+                    Write a Blog
+                  </a>
+                </li>
+              </ul>
             </div>
-          </div>
-        ))}
-      </div>
+          </nav>
+        </header>
+        <div>
+          {TestBlogs.map((blog, index) => (
+            <div key={index}>
+              <div>
+                <div> {blog.title} </div>
+                <div> {blog.body}</div>
+                <div> {blog.publishDate}</div>
+                <div> {blog.author}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </body>
     </>
   );
 }
